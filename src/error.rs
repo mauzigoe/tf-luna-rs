525af195b752e1ab6d4fc8e59971d6cb236eba52
Frangen, Leon <lesoftware@proton.me>
2026-02-22 10:57:32 +0100

    docs: add doc for LidarError

--- src/error.rs
+++ src/error.rs
@@ -3,6 +3,8 @@ use defmt::Format;
 /// Error cases for the TF Luna Lidar Handler
 #[derive(Clone, Debug, Format)]
 pub enum LidarError {
+    /// Request to the Tf Luna Driver failed
     RequestFailed,
+    /// Tf Luna's `W: Write` interface is not initaialized
     WriteInterfaceNotInitialized,
 }

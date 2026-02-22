4bc34b6d1b46e19c1f8b3ae06df102a6a48bf1d7
Frangen, Leon <lesoftware@proton.me>
2026-02-22 10:56:12 +0100

    docs: add doc for module usages in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 //#![warn(missing_docs)]
 #![cfg_attr(not(test), no_std)]
 
+/// Error Codes
 pub mod error;
+/// Handler implementation of the TF Luna Driver
 pub mod handler;
+/// Messages sent and received by the driver
 pub mod message;

3eb6e5a83f0a9abd5cdb92fef2ee0cc0d075b928
Frangen, Leon <lesoftware@proton.me>
2026-03-17 22:23:21 +0100

    doc: add introduction text for documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 #![cfg_attr(not(test), no_std)]
 
+//! Driver implementation for [TF Luna Lidar](https://en.benewake.com/uploadfiles/2024/04/20240426135946148.pdf)
+//!
+//! Features:
+//! - Interface-agnostic: the driver is not implement for a specific UART/I2C driver, but expects the input via a shared read buffer
+//! - Full implementation of the serial communication protocol (Appendix II). All messages are implemented as `struct`s and can be sent/received via the driver
+
 /// Error Codes
 pub mod error;
 /// Handler implementation of the TF Luna Driver
